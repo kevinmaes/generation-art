06cbcdb494180c0acc68eaed05025b049a3aa82f
Kevin Maes <kevin@kevinmaes.com>
2025-06-02 12:26:19 +0200

    Add scripts for prettier and eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,55 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+// See: https://eslint.org/docs/latest/use/configure/configuration-files-new
 
-export default tseslint.config(
-  { ignores: ['dist'] },
-  {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
-    plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
-    },
-    rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
-    },
-  },
-)
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import react from 'eslint-plugin-react';
+import prettier from 'eslint-config-prettier';
+
+/** @type {import('eslint').Linter.FlatConfig[]} */
+export default [
+	js.configs.recommended,
+	{
+		files: ['**/*.ts', '**/*.tsx'],
+		languageOptions: {
+			parser: tseslint.parser,
+			parserOptions: {
+				ecmaVersion: 2020,
+				globals: globals.browser,
+				project: true,
+				tsconfigRootDir: import.meta.dirname,
+			},
+		},
+		plugins: {
+			'@typescript-eslint': tseslint,
+			'react-hooks': reactHooks,
+			'react-refresh': reactRefresh,
+			'react': react,
+		},
+		rules: {
+			...tseslint.configs.recommended.rules,
+			...tseslint.configs.strictTypeChecked.rules,
+			...tseslint.configs.stylisticTypeChecked.rules,
+			...reactHooks.configs.recommended.rules,
+			'react-refresh/only-export-components': [
+				'warn',
+				{ allowConstantExport: true },
+			],
+			'@typescript-eslint/no-explicit-any': 'error',
+			'@typescript-eslint/explicit-function-return-type': 'error',
+			'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+			'@typescript-eslint/consistent-type-imports': 'error',
+			'react/jsx-uses-react': 'error',
+			'react/jsx-uses-vars': 'error',
+			'react/prop-types': 'off',
+			'react/react-in-jsx-scope': 'off',
+			'no-console': ['warn', { allow: ['warn', 'error'] }],
+			'no-debugger': 'warn',
+			'prefer-const': 'error',
+			'no-var': 'error',
+		},
+	},
+	prettier,
+];
